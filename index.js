9042a39db3b4478eb3557d5c5a5bf25f910d1b45
noob <shelcia1999@gmail.com>
2020-09-03 22:24:44 +0530

    socket update

--- index.js
+++ index.js
@@ -39,14 +39,55 @@ const io = socketIo(server);
 
 //SOCKET CONNECTION
 
-io.on("connection", (socket) => {
-  console.log("New Connection !!");
+io.on("connect", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
-    // callback({ error: "error" });
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    //INBUILT FUCNTION JOIN IS CALLED TO JOIN THE USER IN ROOM
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+
+    //BROADCAST IS INBUILT FUNCTION TO SEND MESSAGE TO CONNECTION EXCEPT THE CLIENT WHO SENT IT
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
   });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", () => {
-    console.log("User has left");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
 
--- routes/chat/users.js
+++ routes/chat/users.js
@@ -10,11 +10,15 @@ const addUser = ({ id, name, room }) => {
   const existingUser = users.find(
     (user) => user.room === room && user.name === name
   );
-  if (existingUser) {
-    return { error: "User name is already taken" };
-  }
+
+  if (!name || !room) return { error: "Username and room are required." };
+  if (existingUser) return { error: "Username is taken." };
+
   const user = { id, name, room };
+
   users.push(user);
+
+  return { user };
 };
 
 const removeUser = (id) => {
